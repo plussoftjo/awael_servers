03117e25bed4fa2d33667956763853a320096d1c
plussoftjo <44112848+plussoftjo@users.noreply.github.com>
2020-06-06 17:47:57 +0300

    Update website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,261 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="en">
     <head>
+        <!-- Required meta tags -->
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="welcome/img/favicon.png" type="image/png">
+        <title>شركة الأوائل - لجلب الأيدي العاملة</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="welcome/css/bootstrap.css">
+        <link rel="stylesheet" href="welcome/vendors/linericon/style.css">
+        <link rel="stylesheet" href="welcome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="welcome/vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="welcome/vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="welcome/vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="welcome/vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="welcome/vendors/popup/magnific-popup.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="welcome/css/style.css">
+        <link rel="stylesheet" href="welcome/css/responsive.css">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <body data-spy="scroll" data-target="#mainNav" data-offset="70">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+            <div class="main_menu" id="mainNav">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="#" style="border-radius:30px;"><img width="100" height="100" style="border-radius:30px;" src="welcome/img/icon.png" alt=""></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav ml-auto">
+								<li class="nav-item active"><a class="nav-link" href="#home">Home</a></li>
+								<li class="nav-item"><a class="nav-link" href="#feature">FEATURES</a></li>
+								<li class="nav-item"><a class="nav-link" href="#screen">SCREENS</a>
+							</ul>
+						</div>
+					</div>
+            	</nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
+
+        <!--================Home Banner Area =================-->
+        <section class="home_banner_area" id="home">
+            <div class="banner_inner">
+				<div class="container">
+					<div class="row banner_content">
+						<div class="col-lg-9">
+							<h2>Alawael <br />Application </h2>
+							<p>شركة الاوائل لجلب ايدي العاملة  .</p>
+						</div>
+						<div class="col-lg-3">
+							<div class="banner_map_img">
+								<img class="img-fluid" src="welcome/img/6.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+
+        <!--================Feature Area =================-->
+        <section class="feature_area p_120" id="feature">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Unique Features</h2>
+        		</div>
+        		<div class="feature_inner row">
+        			<div class="col-lg-3 col-md-6">
+        				<div class="feature_item">
+        					<img src="welcome/img/icon/f-icon-1.png" alt="">
+        					<h4>Get worker </h4>
+                            <p>Anywhare you are in</p>
+                        </div>
+        			</div>
+        			<div class="col-lg-3 col-md-6">
+        				<div class="feature_item">
+        					<img src="welcome/img/icon/f-icon-1.png" alt="">
+                            <h4>Done Service quickly </h4>
+                            <p>Help you in the place </p>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-md-6">
+        				<div class="feature_item">
+        					<img src="welcome/img/icon/f-icon-1.png" alt="">
+                            <h4>Services</h4>
+                            <p>all service in the app</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-md-6">
+        				<div class="feature_item">
+        					<img src="welcome/img/icon/f-icon-1.png" alt="">
+                            <h4>Support </h4>
+                            <p>24/7 support </p>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Feature Area =================-->
+
+        <!--================Interior Area =================-->
+        <section class="interior_area interior_two">
+        	<div class="container">
+        		<div class="interior_inner row">
+        			<div class="col-lg-5 offset-lg-1">
+        				<div class="interior_text">
+        					<h4>Based on your service</h4>
+                            <p>شركة الاوائل لجلب الايدي العاملة </p>
+        				</div>
+        			</div>
+        			<div class="col-lg-6">
+        				<img class="img-fluid" width="300" height="700" src="welcome/img/6.png" alt="">
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Interior Area =================-->
+
+        <!--================Feature Area =================-->
+        <section class="screenshot_area p_120" id="screen">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Screenshots</h2>
+                    <p>Screenshots of the app</p>
                 </div>
-            @endif
+        		<div class="screenshot_inner owl-carousel">
+        			<div class="item">
+        				<img src="welcome/img/feature/1.png" alt="">
+        			</div>
+        			<div class="item">
+        				<img src="welcome/img/feature/3.png" alt="">
+        			</div>
+        			<div class="item">
+        				<img src="welcome/img/feature/4.png" alt="">
+        			</div>
+        			<div class="item">
+        				<img src="welcome/img/feature/5.png" alt="">
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Feature Area =================-->
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+
+        <!--================Download App Area =================-->
+        <section class="download_app_area p_120">
+        	<div class="container">
+        		<div class="app_inner">
+        			<h2>Download This App Today!</h2>
+        			<div class="app_btn_area">
+        				<div class="app_btn">
+        					<div class="media">
+								<div class="d-flex">
+									<i class="fa fa-apple" aria-hidden="true"></i>
+								</div>
+								<div class="media-body">
+									<a href=""><h4>Available</h4></a>
+									<p>on App Store</p>
+								</div>
+							</div>
+        				</div>
+        				<div class="app_btn">
+        					<div class="media">
+								<div class="d-flex">
+									<i class="fa fa-android" aria-hidden="true"></i>
+								</div>
+								<div class="media-body">
+									<a href=""><h4>Available</h4></a>
+									<p>on App Store</p>
+								</div>
+							</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Download App Area =================-->
+
+        <!--================ start footer Area  =================-->
+        <footer class="footer-area p_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3  col-md-6 col-sm-6">
+                        <div class="single-footer-widget tp_widgets">
+                           <h6 class="footer_title">Top Products</h6>
+                            <ul class="list">
+                            	<li><a href="#">Managed Website</a></li>
+                            	<li><a href="#">Manage Reputation</a></li>
+                            	<li><a href="#">Power Tools</a></li>
+                            	<li><a href="#">Marketing Service</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-6 col-sm-6">
+                        <div class="single-footer-widget news_widgets">
+                            <h6 class="footer_title">Newsletter</h6>
+                            <p>You can trust us. we only send promo offers, not a single spam.</p>
+                            <div id="mc_embed_signup">
+                                <form target="_blank" action="" method="get" class="subscribe_form relative">
+                                    <div class="input-group d-flex flex-row">
+                                        <input name="EMAIL" placeholder="Your email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+                                        <button class="btn sub-btn">Get Started</button>
+                                    </div>
+                                    <div class="mt-10 info"></div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6 offset-lg-1">
+                        <div class="single-footer-widget instafeed">
+
+                        </div>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="row footer-bottom d-flex justify-content-between align-items-center">
+                    <p class="col-lg-8 col-md-8 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This App Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by Plus Soft jo</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                    <div class="col-lg-4 col-md-4 footer-social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-dribbble"></i></a>
+                        <a href="#"><i class="fa fa-behance"></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </footer>
+		<!--================ End footer Area  =================-->
+
+
+
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="welcome/js/jquery-3.2.1.min.js"></script>
+        <script src="welcome/js/popper.js"></script>
+        <script src="welcome/js/bootstrap.min.js"></script>
+        <script src="welcome/js/stellar.js"></script>
+        <script src="welcome/vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="welcome/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="welcome/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="welcome/vendors/isotope/isotope-min.js"></script>
+        <script src="welcome/vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="welcome/js/jquery.ajaxchimp.min.js"></script>
+        <script src="welcome/vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="welcome/vendors/counter-up/jquery.counterup.js"></script>
+        <script src="welcome/js/mail-script.js"></script>
+        <script src="welcome/vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="welcome/js/theme.js"></script>
     </body>
 </html>
